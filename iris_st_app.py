cdc1a1b425bc19069728c433b35d28bebe4fd376
chirag <61842354+chiragpalan@users.noreply.github.com>
2024-09-07 13:21:41 +0530

    Update iris_st_app.py

--- iris_st_app.py
+++ iris_st_app.py
@@ -13,7 +13,7 @@ st.title("Iris Model Prediction Using Pickle File from GitHub")
 iris = load_iris()
 
 # URL of the pickle file hosted on GitHub
-url = 'https://github.com/chiragpalan/test/blob/main/iris_model.pkl'
+url = 'https://raw.githubusercontent.com/chiragpalan/test/main/iris_model.pkl'
 
 # Function to load the model from GitHub
 @st.cache_resource
