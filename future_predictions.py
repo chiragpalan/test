beac008c5f16ad7026bb601bafcb48f13b4b7f2d
chirag <61842354+chiragpalan@users.noreply.github.com>
2024-11-17 22:27:13 +0530

    Update future_predictions.py

--- future_predictions.py
+++ future_predictions.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler
 import joblib
 import numpy as np
 import xgboost as xgb
-import requests
+import requests 
 
 # Paths and configurations
 DATA_DB = 'joined_data.db'
